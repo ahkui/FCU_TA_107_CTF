4f1dce7909747541e3d6b846a57998fe4445b30c
123ojp <shepherd123ojp@gmail.com>
2018-08-27 23:25:43 +0800

    w10

---
+++ web CTF/W10/server/index.php
@@ -0,0 +1,72 @@
+<?php
+
+    function getIp(){
+      $ip = '';
+      if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+      }else{
+       $ip = $_SERVER['REMOTE_ADDR'];
+      }
+      $ip_arr = explode(',', $ip);
+      return $ip_arr[0];
+
+    }
+    $result = "";
+    $ip = getIp()
+    if  ($ip == "127.0.0.1"){
+      $result = "CTF{are_you_reary_at_127.0.0.1?}";
+    } else{
+      $result = "Your ip is ".$ip." Not from localhost";
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Web 11: Command Injection</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-align: center;
+            align-items: center;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #f5f5f5;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <div class="container" id="main">
+        <h2>Enter Command:</h2>
+        <form method="POST" action="index.php">
+            <div class="form-row align-items-center">
+                <div class="col-sm">
+                    <label class="sr-only" for="inputCommand">Command</label>
+                    <input type="text" class="form-control mb-2" name="inputCommand" placeholder="請輸入指令...">
+                </div>
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <h2>Result:</h2>
+        <pre><code><?php echo htmlspecialchars($result); ?></code></pre>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</body>
+</html>
