b837618cae9e444ecb96439d5676f08567ff9664
123ojp <shepherd123ojp@gmail.com>
2018-08-27 23:27:08 +0800

    w10

--- web CTF/W10/server/index.php
+++ web CTF/W10/server/index.php
@@ -21,13 +21,12 @@
 
 
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Web 11: Command Injection</title>
+    <title>Web 8: Cookie</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <style>
@@ -49,21 +48,9 @@
 </head>
 <body>
     <div class="container" id="main">
-        <h2>Enter Command:</h2>
-        <form method="POST" action="index.php">
-            <div class="form-row align-items-center">
-                <div class="col-sm">
-                    <label class="sr-only" for="inputCommand">Command</label>
-                    <input type="text" class="form-control mb-2" name="inputCommand" placeholder="請輸入指令...">
-                </div>
-                <div class="col-auto">
-                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
-                </div>
-            </div>
-        </form>
-        <hr>
-        <h2>Result:</h2>
-        <pre><code><?php echo htmlspecialchars($result); ?></code></pre>
+        <h1 class="text-center">
+            <?php echo $text;?>
+        </h1>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
