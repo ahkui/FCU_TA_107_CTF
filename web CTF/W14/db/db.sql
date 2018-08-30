05027995dfedc527b7d33a634385ead19b4c68f7
123ojp <shepherd123ojp@gmail.com>
2018-08-31 03:31:57 +0800

    w14 finish

--- web CTF/W14/db/db.sql
+++ web CTF/W14/db/db.sql
@@ -11,7 +11,7 @@ CREATE TABLE `users` (
 
 CREATE TABLE `ctf` (
   `id` int(11) NOT NULL auto_increment,
-  `flag` varchar(64) NOT NULL,
+  `flagggggggg` varchar(64) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -20,4 +20,4 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES ("2", "toor", "aab2858
 INSERT INTO `users` (`id`, `username`, `password`) VALUES ("3", "abcd", "89675f6b47198123940613cd72bc17d9dee284b9");
 INSERT INTO `users` (`id`, `username`, `password`) VALUES ("4", "1234", "e5eb3a782f7299554923fa793c051d80cb2a58af");
 INSERT INTO `users` (`id`, `username`, `password`) VALUES ("5", "a1b2", "a74842ae4d89b5e28a08ba711325276809ebdf37");
-INSERT INTO `ctf` (`id`, `flag`) VALUES ("1", "CTF{W0w!!U_us3_sq1_lnj3ctl0n_t0_g3t_m3!!}");
+INSERT INTO `ctf` (`id`, `flagggggggg`) VALUES ("1", "CTF{W0w!!U_us3_sq1_lnj3ctl0n_t0_g3t_m3!!}");
