08c7ecd0523c285ce616dc0fe716299ee292a651
123ojp <shepherd123ojp@gmail.com>
2018-08-30 23:30:39 +0800

    W12 fix

--- web CTF/W12/server/index.php
+++ web CTF/W12/server/index.php
@@ -97,7 +97,7 @@
             <input id="password" class="form-control" placeholder="Password" value="<?php echo  $_SESSION["nextoken"]?>" type="text" name="token">
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 
-            <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
+          
         </form>
 
     <h1 class="text-center"><?php echo $result; ?></h1>
