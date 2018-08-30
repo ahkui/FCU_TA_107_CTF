8b094f7d34eb98bb83317edf3fbb8348aad51d46
123ojp <shepherd123ojp@gmail.com>
2018-08-30 23:38:57 +0800

    del logo

--- web CTF/W9/server/index.php
+++ web CTF/W9/server/index.php
@@ -82,7 +82,6 @@
         if( !isset($_POST['username']) || !isset($_POST['password']) || $_POST['username']=="" || $_POST['password']=="" ) {
     ?>
         <form class="form-signin text-center" method="POST" action="index.php">
-            <img class="mb-4" src="https://ctf.vongola.pw/img/logo.png" alt="" height="72">
             <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
             <label for="username" class="sr-only">Username</label>
             <input id="username" class="form-control" placeholder="Username" required="" autofocus="" type="text" name="username">
