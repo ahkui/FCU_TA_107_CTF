24d2d224d23c11c515a16b4329fcc23bb71aea5f
123ojp <shepherd123ojp@gmail.com>
2018-08-31 03:30:45 +0800

    w14 finish

--- web CTF/W14/server/login.php
+++ web CTF/W14/server/login.php
@@ -74,6 +74,7 @@
         ?>
 
     </h1>
+    <?php if($success === true): ?>
     <table class="table table-bordered">
         <thead>
           <tr>
@@ -101,7 +102,7 @@
                 }
                  ?>
         </tbody>
-
+  <?php endif; ?>
     </table>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
