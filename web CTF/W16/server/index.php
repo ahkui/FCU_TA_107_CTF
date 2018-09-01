d3d4681c84ba80237da653ff0bde41595d38fdc4
123ojp <shepherd123ojp@gmail.com>
2018-09-01 22:58:20 +0800

    fix W16

--- web CTF/W16/server/index.php
+++ web CTF/W16/server/index.php
@@ -9,9 +9,8 @@
     function is_correct($password) {
         $result = false;
         extract($_GET);
-        $key = $_GET['password']
         # sha1($real_password) === 9de64900e39e30294a5022e39b050c7d39f566ac $pass = "9de64900e39e30294a5022e39b050c7d39f566ac";
-        $key = hash('sha1', $key)
+        $password = hash('sha1', $password);
         if ( $key === $pass ) {
           $result = true;
         }
@@ -25,7 +24,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Web 9: SHA 1</title>
+    <title>Web 16: Extract</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <style>
